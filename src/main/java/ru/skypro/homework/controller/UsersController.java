8df20e970e442435d39f02c25e384a24420c34e5
Far1ck <salyakhov.farit@gmail.com>
2025-11-27 23:43:23 +0500

    feat: add updateAvatar PATCH endpoint

--- src/main/java/ru/skypro/homework/controller/UsersController.java
+++ src/main/java/ru/skypro/homework/controller/UsersController.java
@@ -1,10 +1,22 @@
 package ru.skypro.homework.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
 public class UsersController {
 
+    @PatchMapping("/me/image")
+    public ResponseEntity<?> updateAvatar(@RequestParam("image") MultipartFile image) {
+        if (true) {
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
 }
