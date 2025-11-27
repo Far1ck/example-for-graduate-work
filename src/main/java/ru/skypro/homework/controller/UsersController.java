c0d2284b0d7eb035c3993f4e7fdec67f5ea85e8c
Far1ck <salyakhov.farit@gmail.com>
2025-11-27 23:19:08 +0500

    feat: add updateUser PATCH endpoint and DTO

--- src/main/java/ru/skypro/homework/controller/UsersController.java
+++ src/main/java/ru/skypro/homework/controller/UsersController.java
@@ -1,10 +1,22 @@
 package ru.skypro.homework.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import ru.skypro.homework.dto.UpdateUser;
 
 @RestController
 @RequiredArgsConstructor
 public class UsersController {
 
+    @PatchMapping("/me")
+    public ResponseEntity<UpdateUser> updateUser (@RequestBody UpdateUser updateUser) {
+        if (true) {
+            return ResponseEntity.ok(new UpdateUser());
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
 }
