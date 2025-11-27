57389e1362eae3f952a3f3a99200215a4953a6c9
Far1ck <salyakhov.farit@gmail.com>
2025-11-27 22:33:33 +0500

    feat: add password update endpoint and DTO

--- src/main/java/ru/skypro/homework/controller/UsersController.java
+++ src/main/java/ru/skypro/homework/controller/UsersController.java
@@ -1,10 +1,29 @@
 package ru.skypro.homework.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.skypro.homework.dto.NewPassword;
 
 @RestController
+@RequestMapping("/users")
 @RequiredArgsConstructor
 public class UsersController {
 
+    private final UsersService usersService;
+
+    @PostMapping("/set_password")
+    public ResponseEntity<?> setPassword(@RequestBody NewPassword newPassword) {
+        if () {
+            return ResponseEntity.ok().build();
+        } else if () {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+    }
 }
