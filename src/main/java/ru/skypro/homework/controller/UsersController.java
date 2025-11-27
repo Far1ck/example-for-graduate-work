bb09181a205175527383c2df1c4e4c3088bc2f5f
Far1ck <salyakhov.farit@gmail.com>
2025-11-27 23:02:27 +0500

    feat: add getUser GET endpoint and User DTO

--- src/main/java/ru/skypro/homework/controller/UsersController.java
+++ src/main/java/ru/skypro/homework/controller/UsersController.java
@@ -1,10 +1,23 @@
 package ru.skypro.homework.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.skypro.homework.dto.User;
 
+@CrossOrigin(value = "http://localhost:3000")
 @RestController
 @RequiredArgsConstructor
 public class UsersController {
 
+    @GetMapping("/me")
+    public ResponseEntity<User> getUser() {
+        if (true) {
+            return ResponseEntity.ok(new User());
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
 }
