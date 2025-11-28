15edf5a373f4da4389251c0c44c0d0dc40e0c4d9
Far1ck <salyakhov.farit@gmail.com>
2025-11-28 20:51:50 +0500

    feat: add getAds GET endpoint and DTOs (Ad, Ads)

--- src/main/java/ru/skypro/homework/controller/AdvertisementsController.java
+++ src/main/java/ru/skypro/homework/controller/AdvertisementsController.java
@@ -1,9 +1,12 @@
 package ru.skypro.homework.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.skypro.homework.dto.Ads;
 
 @CrossOrigin(value = "http://localhost:3000")
 @RestController
@@ -11,4 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AdvertisementsController {
 
+    @GetMapping
+    public ResponseEntity<Ads> getAds() {
+        return ResponseEntity.ok(new Ads());
+    }
 }
