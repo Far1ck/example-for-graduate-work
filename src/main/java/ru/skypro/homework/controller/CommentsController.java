89d4424ab7195b07d01cb1dc7da939918db196ba
Far1ck <salyakhov.farit@gmail.com>
2025-11-28 23:59:37 +0500

    feat: add getComments GET endpoint and DTOs

--- src/main/java/ru/skypro/homework/controller/CommentsController.java
+++ src/main/java/ru/skypro/homework/controller/CommentsController.java
@@ -1,9 +1,10 @@
 package ru.skypro.homework.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.skypro.homework.dto.Comments;
 
 @CrossOrigin(value = "http://localhost:3000")
 @RestController
@@ -11,4 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CommentsController {
 
+    @GetMapping("/{id}/comments")
+    public ResponseEntity<Comments> getComments(@PathVariable("id") int id) {
+        if (false) {
+            ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        if (false) {
+            ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(new Comments());
+    }
 }
