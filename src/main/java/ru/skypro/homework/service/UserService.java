d51858f6fcab42823ab53a341b40c95861ccb7d3
Far1ck <salyakhov.farit@gmail.com>
2025-12-12 23:58:10 +0500

    feat: add updateUser method

--- src/main/java/ru/skypro/homework/service/UserService.java
+++ src/main/java/ru/skypro/homework/service/UserService.java
@@ -1,6 +1,7 @@
 package ru.skypro.homework.service;
 
 import ru.skypro.homework.dto.NewPassword;
+import ru.skypro.homework.dto.UpdateUser;
 import ru.skypro.homework.dto.User;
 import ru.skypro.homework.entity.UserEntity;
 
@@ -8,4 +9,5 @@ public interface UserService {
     void createUser(UserEntity user);
     boolean setPassword(String username, NewPassword passwords);
     User getUser(String username);
+    UpdateUser updateUser(String name, UpdateUser updateUser);
 }
