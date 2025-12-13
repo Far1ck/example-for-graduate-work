e2673280ae18b934d32e0ca7746c7a007853a941
Far1ck <salyakhov.farit@gmail.com>
2025-12-13 06:14:48 +0500

    feat: add addComment method

--- src/main/java/ru/skypro/homework/controller/CommentsController.java
+++ src/main/java/ru/skypro/homework/controller/CommentsController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import ru.skypro.homework.dto.Comment;
 import ru.skypro.homework.dto.Comments;
@@ -46,14 +47,13 @@ public class CommentsController {
     @ApiResponse(responseCode = "401", description = "Unauthorized")
     @ApiResponse(responseCode = "404", description = "Not found")
     public ResponseEntity<Comment> addComment(@PathVariable("id") int id,
-                                              @RequestBody CreateOrUpdateComment properties) {
-        if (false) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-        if (false) {
+                                              @RequestBody CreateOrUpdateComment properties,
+                                              Authentication authentication) {
+        Comment comment = commentsService.addComment(authentication.getName(), id, properties);
+        if (comment == null) {
             return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.ok(new Comment());
+        return ResponseEntity.ok(comment);
     }
 
     @DeleteMapping("/{adId}/comments/{commentId}")
